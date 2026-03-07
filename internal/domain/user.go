1504c5d6f53a3cde3f8d3c49049efd47c73d72f5
ruhan <robiuzzaman4@gmail.com>
2026-03-07 15:52:20 +0600

    feat: user domain done

---
+++ internal/domain/user.go
@@ -0,0 +1,51 @@
+package domain
+
+import "time"
+
+type UserBloodGroup string
+type UserRole string
+type UserGender string
+
+const (
+	APositive  UserBloodGroup = "A+"
+	ANegative  UserBloodGroup = "A-"
+	BPositive  UserBloodGroup = "B+"
+	BNegative  UserBloodGroup = "B-"
+	OPositive  UserBloodGroup = "O+"
+	ONegative  UserBloodGroup = "O-"
+	ABPositive UserBloodGroup = "AB+"
+	ABNegative UserBloodGroup = "AB-"
+)
+
+const (
+	RoleAdmin UserRole = "ADMIN"
+	RoleUser  UserRole = "USER"
+)
+const (
+	Male   UserGender = "MALE"
+	Female UserGender = "FEMALE"
+)
+
+type UserAddress struct {
+	Zila         string `json:"zila" db:"zila"`
+	Upazila      string `json:"upazila" db:"upazila"`
+	LocalAddress string `json:"localAddress" db:"local_address"`
+}
+
+type User struct {
+	ID               int            `json:"id" db:"id"`
+	Name             string         `json:"name" db:"name"`
+	Phone            string         `json:"phone" db:"phone"`
+	Password         string         `json:"-" db:"password"`
+	BloodGroup       UserBloodGroup `json:"bloodGroup" db:"blood_group"`
+	Role             UserRole       `json:"role" db:"role"`
+	Gender           UserGender     `json:"gender" db:"gender"`
+	DateOfBirth      time.Time      `json:"dateOfBirth" db:"date_of_birth"`
+	IsVerified       bool           `json:"isVerified" db:"is_verified"`
+	Address          UserAddress    `json:"address" db:"address"`
+	TotalDonateCount int            `json:"totalDonateCount" db:"total_donate_count"`
+	IsAvailable      bool           `json:"isAvailable" db:"is_available"`
+	LastDonatedAt    time.Time      `json:"lastDonatedAt" db:"last_donated_at"`
+	CreatedAt        time.Time      `json:"createdAt" db:"created_at"`
+	UpdatedAt        time.Time      `json:"updatedAt" db:"updated_at"`
+}
