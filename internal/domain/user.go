f43edb8544e0b7f8671036aa15e654f0ea3a64c6
ruhan <robiuzzaman4@gmail.com>
2026-03-14 14:47:36 +0600

    feat: user domain struct updated

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -30,18 +30,18 @@ type User struct {
 	Name             string         `json:"name" db:"name"`
 	Phone            string         `json:"phone" db:"phone"`
 	Password         string         `json:"-" db:"password"`
-	BloodGroup       UserBloodGroup `json:"bloodGroup" db:"blood_group"`
+	BloodGroup       UserBloodGroup `json:"blood_group" db:"blood_group"`
 	Role             UserRole       `json:"role" db:"role"`
 	Gender           UserGender     `json:"gender" db:"gender"`
-	DateOfBirth      time.Time      `json:"dateOfBirth" db:"date_of_birth"`
+	DateOfBirth      time.Time      `json:"date_of_birth" db:"date_of_birth"`
 	Zila             string         `json:"zila" db:"zila"`
 	Upazila          string         `json:"upazila" db:"upazila"`
-	LocalAddress     string         `json:"localAddress" db:"local_address"`
-	TotalDonateCount int            `json:"totalDonateCount" db:"total_donate_count"`
-	IsVerified       bool           `json:"isVerified" db:"is_verified"`
-	IsAvailable      bool           `json:"isAvailable" db:"is_available"`
-	IsDeleted        bool           `json:"IsDeleted" db:"is_deleted"`
-	LastDonatedAt    time.Time      `json:"lastDonatedAt" db:"last_donated_at"`
-	CreatedAt        time.Time      `json:"createdAt" db:"created_at"`
-	UpdatedAt        time.Time      `json:"updatedAt" db:"updated_at"`
+	LocalAddress     string         `json:"local_address" db:"local_address"`
+	TotalDonateCount int            `json:"total_donate_count" db:"total_donate_count"`
+	IsVerified       bool           `json:"is_verified" db:"is_verified"`
+	IsAvailable      bool           `json:"is_available" db:"is_available"`
+	IsDeleted        bool           `json:"is_deleted" db:"is_deleted"`
+	LastDonatedAt    time.Time      `json:"last_donated_at" db:"last_donated_at"`
+	CreatedAt        time.Time      `json:"created_at" db:"created_at"`
+	UpdatedAt        time.Time      `json:"updated_at" db:"updated_at"`
 }
